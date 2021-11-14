f07353ec7c56f983e5a87824dcf51225752edcc5
takosan8 <yukiwebcreate@gmail.com>
2021-11-15 01:26:46 +0900

    redesign 02

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { InputTodo } from "./components/InputTodo";
 import { IncompleteTodos } from "./components/IncompleteTodos";
 import { CompleteTodos } from "./components/CompleteTodos";
+import { Header } from "./components/Header";
 
 import './style.css';
 
@@ -12,6 +13,14 @@ function App() {
     "new Task 2"
   ]);
   const [completeTodos, setCompleteTodos] = useState(["Task C", "Task D"]);
+
+  const [activeTab, setActiveTab] = useState("tab1");
+  const onClickActive1 = () => {
+    setActiveTab("tab1");
+  };
+  const onClickActive2 = () => {
+    setActiveTab("tab2");
+  };
 
   const onChangeTodoText = (e) => setTodoText(e.target.value);
   const onClickAdd = () => {
@@ -25,6 +34,15 @@ function App() {
     newTodos.splice(index, 1);
     setIncompleteTodos(newTodos);
   };
+  const onClickOngoing = (index) => {
+    const arr = document.querySelectorAll(".list-row");
+    console.log(arr);
+    if(arr[index].classList.contains('ongoing') == true){
+      arr[index].classList.remove('ongoing');
+    } else {
+      arr[index].classList.add('ongoing');
+    }
+  };
   const onClickComplete = (index) => {
     const newIncompleteTodos = [...incompleteTodos];
     newIncompleteTodos.splice(index, 1);
@@ -32,6 +50,7 @@ function App() {
     setIncompleteTodos(newIncompleteTodos);
     setCompleteTodos(newCompleteTodos);
   };
+
   const onClickBack = (index) => {
     const newCompleteTodos = [...completeTodos];
     newCompleteTodos.splice(index, 1);
@@ -42,27 +61,50 @@ function App() {
   };
   return (
     <>
-    <InputTodo
-      todoText={todoText}
-      onChange={onChangeTodoText}
-      onClick={onClickAdd}
-      disabled={incompleteTodos.length >= 5}
-    />
-    {incompleteTodos.length >= 5 && (
-      <p>
-        You can register todo upto 5.
-        <br />
-        Now it's time to focus finish remaining task.
-      </p>
-    )}
-
-    <IncompleteTodos
-      todos={incompleteTodos}
-      onClickComplete={onClickComplete}
-      onClickDelete={onClickDelete}
-    />
-    <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
-  </>
+      {/* show header */}
+      <Header />
+      <div className="button-area">
+        <button
+          className={activeTab === "tab1" ? "active-btn" : "inactive-btn"}
+          onClick={onClickActive1}
+        >
+          Incomplete
+        </button>
+        <button
+          className={activeTab === "tab2" ? "active-btn" : "inactive-btn"}
+          onClick={onClickActive2}
+        >
+          Done
+        </button>
+      </div>
+      {/* input new task */}
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
+      />
+      {incompleteTodos.length >= 5 && (
+        <p className="overAlert">
+          You can register todo upto 5.
+          <br />
+          Now it's time to focus finish remaining task.
+        </p>
+      )}
+      <div className={activeTab === "tab1" ? "tab tab-done active" : "tab"}>
+        <IncompleteTodos
+          todos={incompleteTodos}
+          onClickComplete={onClickComplete}
+          onClickDelete={onClickDelete}
+          onClickOngoing={onClickOngoing}
+        />
+      </div>
+      <div
+        className={activeTab === "tab2" ? "tab tab-incomplete active" : "tab"}
+      >
+        <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
+      </div>
+    </>
   );
 }
 
